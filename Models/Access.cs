2ff76a181ba11956718e4e9df2afacc1cebeb8a3
marcellipet <marcellipetranel@gmail.com>
2025-06-10 14:16:28 -0300

    fix: começando a implmentar jwt da maneira correta

--- Models/Access.cs
+++ Models/Access.cs
@@ -4,6 +4,6 @@ namespace LojaProdutosV2.Models
 {
     public class Access
     {
-
-    }
+        public static string PrivateKey { get; set; } = "5bZ£JU6},£!FUvlimGDVnR6:H~OA";
+    } 
 }
---
+++ Services/TokenService.cs
@@ -0,0 +1,31 @@
+﻿//using System.IdentityModel.Tokens.Jwt;
+//using System.Text;
+//using LojaProdutos.Models;
+//using Microsoft.IdentityModel.Tokens;
+//using System.IdentityModel.Tokens.Jwt;
+
+//namespace LojaProdutosV2.Services
+//{
+//    public class TokenService
+//    {
+//        public string Generate(UsrUsuario usrUsuario)
+//        {
+//            var handler = new JwtSecurityTokenHandler();
+//            var key = Encoding.UTF8.GetBytes(Configuration.Private);
+
+//            var credentials = new SigningCredentials
+//                (new SymmetricSecurityKey(key),
+//                algorithm: SecurityAlgorithms.HmacSha256Signature);
+
+//            var tokenDescriptor = new SecurityTokenDescriptor
+//            {
+//                SigningCredentials = credentials,
+//                Expires = DateTime.UtcNow.AddHours(2)
+//            };
+
+//            var token = handler.CreateToken(tokenDescriptor);
+
+//            return handler.WriteToken(token);
+//        }
+//    }
+//}
