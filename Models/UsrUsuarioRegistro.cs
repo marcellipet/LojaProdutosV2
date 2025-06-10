4afe40e365d794bcb8e9b6606c695083bccbc345
marcellipet <marcellipetranel@gmail.com>
2025-06-10 14:56:05 -0300

    create: registro de usuario

---
+++ Models/UsrUsuarioRegistro.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LojaProdutosV2.Models
+{
+    public class UsrUsuarioRegistro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Senha { get; set; }
+    }
+}
