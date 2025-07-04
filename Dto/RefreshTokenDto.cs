6151a218762ace83823150f2456f0694df44df3d
marcellipet <marcellipetranel@gmail.com>
2025-07-04 16:27:52 -0300

    fix

--- Dto/RefreshTokenDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LojaProdutosV2.Dto
-{
-    public class RefreshTokenDto
-    {
-        public string Token { get; set; } = string.Empty;
-        public string HashRefresh { get; set; }
-    }
-   
-}
