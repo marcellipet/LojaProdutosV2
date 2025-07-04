cfb78e2715eb46fbca7ee6296133275b5a4429d6
marcellipet <marcellipetranel@gmail.com>
2025-07-04 13:58:41 -0300

    create: segregação feita

--- Models/Access.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-
-namespace LojaProdutosV2.Models
-{
-    public class Access
-    {
-        public static string PrivateKey { get; set; } = "5bZ£JU6},£!FUvlimGDVnR6:H~OA";
-    } 
-}
